53fabb85fd6ca568567fb876e90dbecbf303b4e1
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-06-14 14:18:29 -0300

    Criação do service responsável pela comunicação com a rota-api

---
+++ src/app/rotas/service/rota.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Rota } from 'src/app/core/types/rota';
+import { RotaDTO } from 'src/app/core/types/rotaDTO';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RotaService {
+
+  private apiUrl: string = environment.apiRotaUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getRotas(): Observable<Rota[]> {
+    return this.http.get<Rota[]>(`${this.apiUrl}/v1/rota`);
+  }
+
+  getRota(id: number): Observable<RotaDTO> {
+    return this.http.get<RotaDTO>(`${this.apiUrl}/v1/rota/${id}`);
+  }
+
+  createRota(rota: RotaDTO): Observable<Rota> {
+    return this.http.post<Rota>(`${this.apiUrl}/v1/rota`, rota);
+  }
+
+  updateRota(id: number, rota: RotaDTO): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/v1/rota/${id}`, rota);
+  }
+
+  deleteRota(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/v1/rota/${id}`);
+  }
+
+  getMelhorRota(origem: string, destino: string): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/v1/rota/consulta?origem=${origem}&destino=${destino}`);
+  }
+}
